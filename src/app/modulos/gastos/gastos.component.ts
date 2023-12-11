081f27bcb772e91b777988343beda5e61a460633
edinpaco <edinsonalon@gmail.com>
2023-12-11 14:50:20 -0500

    actualizacion modulos

--- src/app/modulos/gastos/gastos.component.ts
+++ src/app/modulos/gastos/gastos.component.ts
@@ -1,17 +1,15 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { SoportesService } from 'src/app/services/soportes.service';
 import { Soporte } from 'src/app/interfaces/soportes';
 import { ToastrService } from 'ngx-toastr';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-declare const bootstrap: any;
 
 @Component({
   selector: 'app-gastos',
   templateUrl: './gastos.component.html',
   styleUrls: ['./gastos.component.css']
 })
-export class GastosComponent {
+export class GastosComponent implements AfterViewInit, OnDestroy {
   id: number;
 
   formSuports: FormGroup;
@@ -19,10 +17,8 @@ export class GastosComponent {
 
   listarSoportes: Soporte[] = [];
 
-  constructor(private _SoportesService: SoportesService, private toastr: ToastrService, private fb: FormBuilder, private el: ElementRef) { 
-
-  //Formulario para Insertar soportes en la base de datos
-
+  constructor(private _SoportesService: SoportesService, private toastr: ToastrService, private fb: FormBuilder, private el: ElementRef) {
+    
     this.formSuports = this.fb.group({
       fecha: ['', Validators.required],
       razon_social: ['', Validators.required],
@@ -35,8 +31,6 @@ export class GastosComponent {
       valor: ['', Validators.required]
     });
 
-//Formulario para editar soportes en la base de datos
-
     this.formEdit = this.fb.group({
       fecha: ['', Validators.required],
       razon_social: ['', Validators.required],
@@ -47,80 +41,34 @@ export class GastosComponent {
       concepto: ['', Validators.required],
       descripcion: ['', Validators.required],
       valor: ['', Validators.required]
-    });   
-
-   
+    });
   }
 
   ngOnInit() {
     this.getListarSoportes();
   }
 
+  async addSupport() {
+    if (this.formSuports.valid) {
+      const supportData: Soporte = { ...this.formSuports.value, documento: this.formSuports.value.documento };
 
-// Función para ingresar soporte en la base de datos
-
-addSuport() {
-  // Verifica si el formulario es válido
-  if (this.formSuports.valid) {
-    const Soporte: Soporte = {
-      fecha: this.formSuports.value.fecha,
-      razon_social: this.formSuports.value.razon_social,
-      nit_cc: this.formSuports.value.nit_cc,
-      guia: this.formSuports.value.guia,
-      factura: this.formSuports.value.factura,
-      proyecto: this.formSuports.value.proyecto,
-      concepto: this.formSuports.value.concepto,
-      descripcion: this.formSuports.value.descripcion,
-      valor: this.formSuports.value.valor,
-      documento: this.formSuports.value.documento
-    }
-
-    this._SoportesService.saveSoporte(Soporte).subscribe(
-      () => {
-        // Limpia los campos del formulario
+      try {
+        await this._SoportesService.saveSoporte(supportData).toPromise();
         this.formSuports.reset();
-
-        // Cierra el modal utilizando JavaScript puro
-        const modalElement = this.el.nativeElement.querySelector('#AddModal');
-        
-        console.log('Modal Element:', modalElement);
-
-        if (modalElement) {
-          const modal = new bootstrap.Modal(modalElement);
-          modal.hide();
-          console.log('Modal ocultado con éxito');
-        } else {
-          console.error('No se encontró el elemento del modal');
-        }
-
-
-        // Recarga la lista de soportes
         this.getListarSoportes();
-
-        // Muestra el toastr después de cerrar el modal
         this.toastr.success('El soporte fue guardado con éxito', 'Soporte Guardado');
-      },
-      (error) => {
-        // Maneja el error, muestra un toastr, o realiza otras acciones según sea necesario
-        console.error('Error al guardar el soporte:', error);
+      } catch (error) {
         this.toastr.error('Hubo un error al guardar el soporte', 'Error');
       }
-    );
-  } else {
-    // Muestra un toastr indicando que el formulario es inválido
-    this.toastr.error('Por favor, complete todos los campos antes de enviar el formulario.', 'Formulario Inválido');
+    } else {
+      this.toastr.error('Por favor, complete todos los campos antes de enviar el formulario.', 'Formulario Inválido');
+    }
   }
-}
 
+  //funcion para obtener los datos del id consultado
 
-
-
-
-// Función para editar soporte en la base de datos
-
-getSoporte(id: number){
-
-  this._SoportesService.getSoporte(id).subscribe((data:Soporte) => {
+  getSoporte(id: number) {
+    this._SoportesService.getSoporte(id).subscribe((data: Soporte) => {
       this.formEdit.setValue({
         fecha: data.fecha,
         razon_social: data.razon_social,
@@ -132,84 +80,74 @@ getSoporte(id: number){
         descripcion: data.descripcion,
         valor: data.valor
       });
-     
     });
   }
+  
+  //funcion para obtener el id que vamos a editar
 
   setEditId(id: number) {
     this.id = id;
-    this.getSoporte(id); // Asegúrate de obtener los datos del soporte antes de abrir el modal de edición
+    this.getSoporte(id);
+  }
+
+  //Funcion para Editar soportes
+
+  async editSuport() {
+    if (this.formEdit.valid) {
+    const idActualizar = this.id;
   
-    // Obtener los datos del soporte y asignarlos al formulario de edición
-  this._SoportesService.getSoporte(id).subscribe((data: Soporte) => {
-    this.formEdit.setValue({
-      fecha: data.fecha,
-      razon_social: data.razon_social,
-      nit_cc: data.nit_cc,
-      guia: data.guia,
-      factura: data.factura,
-      proyecto: data.proyecto,
-      concepto: data.concepto,
-      descripcion: data.descripcion,
-      valor: data.valor
-    });
-  });
-}
-  
+    try {
+      await this._SoportesService.updateSoporte(idActualizar, this.formEdit.value).toPromise();
+      this.getListarSoportes();
 
-
-// Función para editar soporte en la base de datos
-
-editSuport() {
-  const idActualizar = this.id;
-
-  this._SoportesService.updateSoporte(idActualizar, this.formEdit.value).subscribe(() => {
-    this.getListarSoportes(); // Actualiza la lista localmente sin recargar la página
-    //window.location.reload();
-    setTimeout(() => {
       this.toastr.success('El soporte fue actualizado con éxito', 'Soporte Actualizado');
-    }, 1000);
-
-  });
-}
+    } catch (error) {
+      // Maneja el error, por ejemplo, mostrando un mensaje de error
+      this.toastr.error('Hubo un error al actualizar el soporte', 'Error');
+    }
+  } else {
+    this.toastr.error('Por favor, complete todos los campos antes de enviar el formulario.', 'Formulario Inválido');
+  }
+  }
   
 
-// Función para listar soporte en la base de datos
-
-  getListarSoportes(){
+  getListarSoportes() {
     this._SoportesService.getListarSoportes().subscribe((data) => {
       this.listarSoportes = data;
-      
-    })
+      this.destroyAndReinitializeDataTables();
+    });
   }
 
-
-
-// Función para eliminar soporte en la base de datos
-
-  deleteSupport(id: number){
+  deleteSupport(id: number) {
     this._SoportesService.deleteSoporte(id).subscribe(() => {
-      this.getListarSoportes(); // Actualiza la lista localmente sin recargar la página
-      this.toastr.warning('El soporte fue eliminado con exito', 'Soporte Eliminado');
       this.getListarSoportes();
-      
-    })
+      this.toastr.warning('El soporte fue eliminado con éxito', 'Soporte Eliminado');
+    });
   }
-
-  
-  // Función para la tabla de los soportes en la base de datos
 
   ngAfterViewInit() {
-    setTimeout(() => {
+    this.initializeDataTables();
+  }
 
-    // Configura DataTables
+  ngOnDestroy() {
+    $('.tablagastos').DataTable().destroy();
+  }
+
+  private destroyAndReinitializeDataTables() {
+    $('.tablagastos').DataTable().destroy();
+    this.initializeDataTables();
+  }
+
+  private initializeDataTables() {
+    if (this.listarSoportes && this.listarSoportes.length > 0) {
+      setTimeout(() => {
     $('.tablagastos').DataTable({
       searching: true,
       lengthChange: true,
       "language": {
         "search": "Buscar:",
         "lengthMenu": "Mostrar _MENU_ Gastos",
-        "info": "",
+        "info": "Total _TOTAL_ de _MAX_ Gastos",
         "infoEmpty": "Total 0 de 0 Gastos",
         "infoFiltered": "Total filtrado _TOTAL_ de _MAX_ Soportes",
         "zeroRecords": "No se encontraron Soportes",
@@ -221,15 +159,11 @@ editSuport() {
         },
       }
     });
-  }, 100);
-}
-
-truncateText(text: string, maxLength: number): string {
-  if (text.length > maxLength) {
-    return text.substring(0, maxLength - 3) + '...';
-  } else {
-    return text;
+  }, 200);
   }
-}
+  }
 
+  truncateText(text: string, maxLength: number): string {
+    return text.length > maxLength ? text.substring(0, maxLength - 3) + '...' : text;
+  }
 }
